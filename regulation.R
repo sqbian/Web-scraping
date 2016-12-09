bb59fc59ebced03dda21688f6058a971afb4f3e4
sqbian <sqbian@ucdavis.edu>
2016-12-09 11:51:12 -0800

    Update regulation.R

--- regulation.R
+++ regulation.R
@@ -79,3 +79,99 @@ names(finalform) = c('URL','Comment','Name_of_PDF')
 write.csv(finalform,'regulation.csv',row.names = FALSE)
 
 
+
+===================================================================entity extraction==============================================
+library(rJava)
+library(NLP)
+library(openNLP)
+library(RWeka)
+library(XML)
+library(methods)
+
+
+regulation = read.csv('regulation.csv')
+
+word_ann <- Maxent_Word_Token_Annotator()
+sent_ann <- Maxent_Sent_Token_Annotator()
+
+
+person_ann <- Maxent_Entity_Annotator(kind = "person")
+location_ann <- Maxent_Entity_Annotator(kind = "location")
+
+organization_ann <- Maxent_Entity_Annotator(kind = "organization")
+
+pipeline <- list(sent_ann,
+                 word_ann,
+                 person_ann,
+                 location_ann,
+                 organization_ann)
+entities <- function(doc, kind) {
+  s <- doc$content
+  a <- annotations(doc)[[1]]
+  if(hasArg(kind)) {
+    k <- sapply(a$features, `[[`, "kind")
+    s[a[k == kind]]
+  } else {
+    s[a[a$type == "entity"]]
+  }
+}
+
+formdf = function(rownum, kind,a_doc,regulation){
+  entity = entities(a_doc,kind = kind)
+  if(length(entity) == 0){
+    entity = NA
+  }
+  if(length(entity) > 1){
+    entity = paste(unique(entity),collapse = "; ")
+  }
+  regulation[kind][rownum,] = entity
+  regulation
+}
+
+regulation['location'] = rep(NA,dim(regulation)[1])
+regulation['person'] = rep(NA,dim(regulation)[1])
+regulation['organization'] = rep(NA,dim(regulation)[1])
+
+
+  for(i in 1:dim(regulation)[1]){
+  a = as.character(regulation[i,'Comment'])
+  if(a == ""){
+    next
+  }
+  a_annotations <- annotate(a, pipeline)
+  a_doc <- AnnotatedPlainTextDocument(a, a_annotations)
+  regulation = formdf(i,'person',a_doc,regulation)
+  regulation = formdf(i,'location',a_doc,regulation)
+  regulation = formdf(i,'organization',a_doc,regulation)
+  print(i)
+}
+
+
+write.csv(regulation,'regulation_entity.csv',row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
